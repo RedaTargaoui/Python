0455ca3a151ebad918fcf6874efd3a593f3fb83d
ReDa <76877668+RedaTargaoui@users.noreply.github.com>
2021-01-23 16:59:35 +0100

    Create pie_chart.py

---
+++ pie_chart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+y = np.array([35,25,25,15])
+mylabels = ["Python","Java", "C++" , "Javascript"]
+myexplode = [0.2,0,0,0]
+plt.pie(y,labels=mylabels, explode=myexplode , shadow=True)
+plt.show()
+        
